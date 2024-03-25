c6ad69a4996e9c58bc80aaf80fe752e27d5f74c1
maxLiu <neoliukai@gmail.com>
2024-03-25 21:05:26 +0800

    Results score. - Style the rank display with bold font, larger size, and a distinct color.
    feat(App): add ranking system based on reaction time
    
    - Implement a dynamic ranking system in the `AppResults` component.
    - Display user's rank as "ninja fingers", "Rapid Reflexes", or "snail pace..." based on their reaction time

--- src/components/AppResults.vue
+++ src/components/AppResults.vue
@@ -1,9 +1,32 @@
 <template>
   <p>Reaction time - {{ score }} ms</p>
+  <p class="rank">{{ rank }}</p>
 </template>
 
 <script>
 export default {
-  props: ['score']
+  props: ['score'],
+  data() {
+    return {
+      rank: null
+    }
+  },
+  mounted() {
+    if(this.score<250) {
+      this.rank = "ninja fingers"
+    } else if(this.score < 400) {
+      this.rank = "Rapid Reflexes"
+    } else {
+      this.rank = "snail pace..."
+    }
+  }
 }
 </script>
+
+<style>
+.rank {
+  font-weight: bold;
+  font-size: 1.4rem;
+  color: #0faf87;
+}
+</style>
